989821acf266b426b378701730efbf5ed7553868
manhnth <n.t.manh@viscirclevn.com>
2023-11-13 13:06:41 +0700

    initial commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // Disabling on production builds because we're running checks on PRs via GitHub Actions.
+    ignoreDuringBuilds: true
+  }
+}
+
+module.exports = nextConfig
